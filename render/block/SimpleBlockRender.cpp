d31c51b0b96fb7cc678e1d88a9da37cb65e205d6
dar <darek.stojaczyk@gmail.com>
2015-11-25 16:51:34 +0100

    Added "this->" where possible

--- render/block/SimpleBlockRender.cpp
+++ render/block/SimpleBlockRender.cpp
@@ -11,28 +11,28 @@
 
 SimpleBlockRender::SimpleBlockRender() {
     /* initializing square's vertices */
-    vertices[0] = 0.0f;
-    vertices[1] = 1.0f;
+    this->vertices[0] = 0.0f;
+    this->vertices[1] = 1.0f;
 
-    vertices[3] = 0.0f;
-    vertices[4] = 0.0f;
+    this->vertices[3] = 0.0f;
+    this->vertices[4] = 0.0f;
 
-    vertices[6] = 1.0f;
-    vertices[7] = 1.0f;
+    this->vertices[6] = 1.0f;
+    this->vertices[7] = 1.0f;
 
-    vertices[9] = 1.0f;
-    vertices[10] = 0.0f;
+    this->vertices[9] = 1.0f;
+    this->vertices[10] = 0.0f;
 
     /* 3rd dimension vertices (unused) */
-    vertices[2] = vertices[5] = vertices[8] = vertices[11] = 0.0f;
+    this->vertices[2] = this->vertices[5] = this->vertices[8] = this->vertices[11] = 0.0f;
 
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(2, vbo);
+    glGenVertexArrays(1, &this->vao);
+    glGenBuffers(2, this->vbo);
 
-    glBindVertexArray(vao);
+    glBindVertexArray(this->vao);
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); /* vertices vbo */
-    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, this->vbo[0]); /* vertices vbo */
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), this->vertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
@@ -43,40 +43,40 @@ SimpleBlockRender::SimpleBlockRender() {
             0.0f, 1.0f,
     };
 
-    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); /* texture coords vbo */
+    glBindBuffer(GL_ARRAY_BUFFER, this->vbo[1]); /* texture coords vbo */
     glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), tCoords, GL_STATIC_DRAW);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
     glBindVertexArray(0);
 
-    vertShader.load("shader.vert", GL_VERTEX_SHADER);
-    fragShader.load("shader.frag", GL_FRAGMENT_SHADER);
+    this->vertShader.load("shader.vert", GL_VERTEX_SHADER);
+    this->fragShader.load("shader.frag", GL_FRAGMENT_SHADER);
 
-    shaderProgram.createProgram();
-    shaderProgram.addShaderToProgram(&vertShader);
-    shaderProgram.addShaderToProgram(&fragShader);
+    this->shaderProgram.createProgram();
+    this->shaderProgram.addShaderToProgram(&this->vertShader);
+    this->shaderProgram.addShaderToProgram(&this->fragShader);
 
-    shaderProgram.linkProgram();
-    shaderProgram.useProgram();
-    shaderProgram.setUniform("gSampler", 10); //todo hardcoded variable
+    this->shaderProgram.linkProgram();
+    this->shaderProgram.useProgram();
+    this->shaderProgram.setUniform("gSampler", 10); //todo hardcoded variable
 }
 
 void SimpleBlockRender::render(const Block *const block, glm::mat4 projectionMatrix, glm::mat4 viewMatrix) {
-    shaderProgram.useProgram();
-    shaderProgram.setUniform("projectionMatrix", projectionMatrix);
-    tmpModelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f - block->getX() * blockSize * generalScale, 0.0f - block->getY() * blockSize * generalScale, 0.0f));
-    tmpModelMatrix = glm::scale(tmpModelMatrix, glm::vec3(blockSize * generalScale, blockSize * generalScale, 1.0f));
-    tmpModelMatrix = glm::rotate(tmpModelMatrix, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f)); // Just a variation of first rotating
+    this->shaderProgram.useProgram();
+    this->shaderProgram.setUniform("projectionMatrix", projectionMatrix);
+    this->tmpModelMatrix = glm::translate(this->modelMatrix, glm::vec3(0.0f - block->getX() * blockSize * generalScale, 0.0f - block->getY() * blockSize * generalScale, 0.0f));
+    this->tmpModelMatrix = glm::scale(this->tmpModelMatrix, glm::vec3(blockSize * generalScale, blockSize * generalScale, 1.0f));
+    this->tmpModelMatrix = glm::rotate(this->tmpModelMatrix, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f)); // Just a variation of first rotating
 
-    shaderProgram.setUniform("modelViewMatrix", viewMatrix * tmpModelMatrix);
+    shaderProgram.setUniform("modelViewMatrix", viewMatrix * this->tmpModelMatrix);
 
-    glBindVertexArray(vao);
+    glBindVertexArray(this->vao);
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 SimpleBlockRender::~SimpleBlockRender() {
-    glDeleteBuffers(2, vbo);
-    glDeleteVertexArrays(1, &vao);
-    shaderProgram.deleteProgram();
+    glDeleteBuffers(2, this->vbo);
+    glDeleteVertexArrays(1, &this->vao);
+    this->shaderProgram.deleteProgram();
 }
