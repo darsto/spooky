915a4d5546b194a5ff53c97fdda444b3c2fb8212
dar <darek.stojaczyk@gmail.com>
2015-12-25 14:36:32 +0100

    ShapeRender now takes rotation angle into advice

--- src/render/entity/SimpleShapeRender.h
+++ src/render/entity/SimpleShapeRender.h
@@ -13,22 +13,26 @@ public:
     SimpleShapeRender() : DefaultEntityRender("shapes", "shader") { }
 
     virtual void render(const Entity *const entity, glm::mat4 projectionMatrix, glm::mat4 viewMatrix, double scale) override {
-            this->texture.bindTexture(0);
-            this->shaderProgram.useProgram();
-            this->shaderProgram.setUniform("projectionMatrix", projectionMatrix);
-            this->shaderProgram.setUniform("gSampler", texture.getBoundId());
+        this->texture.bindTexture(0);
+        this->shaderProgram.useProgram();
+        this->shaderProgram.setUniform("projectionMatrix", projectionMatrix);
+        this->shaderProgram.setUniform("gSampler", texture.getBoundId());
 
-            this->tmpModelMatrix = glm::translate(this->modelMatrix,
-                                                  glm::vec3(0.0f - (entity->getX() - entity->getWidth() / 2) * scale, 0.0f - (entity->getY() - entity->getHeight() / 2) * scale, 0.0f));
-            this->tmpModelMatrix = glm::scale(this->tmpModelMatrix, glm::vec3(scale, scale, 1.0f));
-            this->tmpModelMatrix = glm::rotate(this->tmpModelMatrix, 0.0f, glm::vec3(1.0f, 0.0f, 0.0f)); // Just a variation of first rotating
+        this->tmpModelMatrix = glm::translate(this->modelMatrix,
+                                              glm::vec3(0.0f - (entity->getX() - entity->getWidth() / 2) * scale, 0.0f - (entity->getY() - entity->getHeight() / 2) * scale, 0.0f));
 
-            shaderProgram.setUniform("modelViewMatrix", viewMatrix * this->tmpModelMatrix);
-            shaderProgram.setUniform("texPosX", (float) (((const SimpleShape *const) entity)->getTexPos() % atlasSize) / atlasSize);
-            shaderProgram.setUniform("texPosY", (float) (((const SimpleShape *const) entity)->getTexPos() / atlasSize) / atlasSize);
+        this->tmpModelMatrix = glm::translate(this->tmpModelMatrix, glm::vec3(0.5 * scale, 0.5 * scale, 0.0)); // Translate to the middle of the entity
+        this->tmpModelMatrix = glm::rotate(this->tmpModelMatrix, (const float) entity->getAngle(), glm::vec3(0.0f, 0.0f, 1.0f)); // Apply rotation
+        this->tmpModelMatrix = glm::translate(this->tmpModelMatrix, glm::vec3(-0.5 * scale, -0.5 * scale, 0.0)); // Translate back to the origin
 
-            glBindVertexArray(this->vao);
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        this->tmpModelMatrix = glm::scale(this->tmpModelMatrix, glm::vec3(scale, scale, 1.0f));
+
+        shaderProgram.setUniform("modelViewMatrix", viewMatrix * this->tmpModelMatrix);
+        shaderProgram.setUniform("texPosX", (float) (((const SimpleShape *const) entity)->getTexPos() % atlasSize) / atlasSize);
+        shaderProgram.setUniform("texPosY", (float) (((const SimpleShape *const) entity)->getTexPos() / atlasSize) / atlasSize);
+
+        glBindVertexArray(this->vao);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     }
 };
 
