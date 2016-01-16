2c12ecb302147183f707b0aa37d98dbb1a775445
dar <darek.stojaczyk@gmail.com>
2016-01-16 18:36:17 +0100

    Redefined VAO gl functions

---
+++ src/render/opengl.cpp
@@ -0,0 +1,23 @@
+//
+// Created by dar on 1/16/16.
+//
+
+#include "opengl.h"
+
+#ifdef __ANDROID__
+
+PFNGLGENVERTEXARRAYSOESPROC glGenVertexArraysOES;
+PFNGLBINDVERTEXARRAYOESPROC glBindVertexArrayOES;
+PFNGLDELETEVERTEXARRAYSOESPROC glDeleteVertexArraysOES;
+PFNGLISVERTEXARRAYOESPROC glIsVertexArrayOES;
+
+void initBindings() {
+    void *libhandle = dlopen("libGLESv2.so", RTLD_LAZY);
+
+    glGenVertexArraysOES = (PFNGLGENVERTEXARRAYSOESPROC) dlsym(libhandle, "glGenVertexArraysOES");
+    glBindVertexArrayOES = (PFNGLBINDVERTEXARRAYOESPROC) dlsym(libhandle, "glBindVertexArrayOES");
+    glDeleteVertexArraysOES = (PFNGLDELETEVERTEXARRAYSOESPROC) dlsym(libhandle, "glDeleteVertexArraysOES");
+    glIsVertexArrayOES = (PFNGLISVERTEXARRAYOESPROC) dlsym(libhandle, "glIsVertexArrayOES");
+}
+
+#endif // __ANDROID__
--- src/render/opengl.h
+++ src/render/opengl.h
@@ -6,13 +6,18 @@
 #define C003_OPENGL_H
 
 #ifdef __ANDROID__
-#define GL_GLEXT_PROTOTYPES
+#include <dlfcn.h>
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #define glBindVertexArray glBindVertexArrayOES
 #define glDeleteVertexArrays glDeleteVertexArraysOES
 #define glGenVertexArrays glGenVertexArraysOES
 #define glIsVertexArray glIsVertexArrayOES
+extern PFNGLGENVERTEXARRAYSOESPROC glGenVertexArraysOES;
+extern PFNGLBINDVERTEXARRAYOESPROC glBindVertexArrayOES;
+extern PFNGLDELETEVERTEXARRAYSOESPROC glDeleteVertexArraysOES;
+extern PFNGLISVERTEXARRAYOESPROC glIsVertexArrayOES;
+void initBindings();
 #else
 #define GLEW_STATIC
 
