d495f2bc9d9a6bcdc8c1dd55413bc5a3e4f69f41
dar <darek.stojaczyk@gmail.com>
2016-01-27 16:03:39 +0100

    Inherit GuiElement, added get&set for "pressed"

--- src/gui/GuiButton.h
+++ src/gui/GuiButton.h
@@ -6,17 +6,24 @@
 #define C003_GUIBUTTON_H
 
 #include <functional>
+#include "GuiElement.h"
 
-class GuiButton {
+class GuiButton : public GuiElement {
 
 public:
     GuiButton(int x, int y, int width, int height);
     virtual bool onClick(int action, float x, float y);
     virtual void setOnClickListener(std::function<bool(int, float, float)> onClickListener);
 
+    bool isPressed() const {
+        return pressed;
+    }
+
+    void setPressed(bool pressed) {
+        GuiButton::pressed = pressed;
+    }
+
 protected:
-    int x, y;
-    int width, height;
     bool pressed;
     std::function<bool(int, float, float)> onClickListener;
 };
