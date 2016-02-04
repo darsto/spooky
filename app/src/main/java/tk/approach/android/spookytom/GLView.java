4b9349cfa26a871b826b042c68e7fd716553f188
dar <darek.stojaczyk@gmail.com>
2016-02-04 19:33:20 +0100

    Fixed multitouch support

--- app/src/main/java/tk/approach/android/spookytom/GLView.java
+++ app/src/main/java/tk/approach/android/spookytom/GLView.java
@@ -18,10 +18,23 @@ public class GLView extends GLSurfaceView {
         setRenderer(renderer);
     }
 
+    private int normalizeMotionAction(int action) {
+        if (action == MotionEvent.ACTION_POINTER_DOWN) return 0;
+        if (action == MotionEvent.ACTION_POINTER_UP) return 1;
+        if (action == MotionEvent.ACTION_CANCEL) return 1;
+        return action;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent e) {
         if (this.renderer.getJniBridge() != null) {
-            this.renderer.getJniBridge().handleTouch(e.getActionIndex(), e.getAction(), e.getX(e.getActionIndex()), e.getY(e.getActionIndex()));
+            if (e.getActionMasked() == MotionEvent.ACTION_MOVE) {
+                for (int i = 0; i < e.getPointerCount(); i++) {
+                    this.renderer.getJniBridge().handleTouch(e.getPointerId(i), normalizeMotionAction(e.getActionMasked()), e.getX(i), e.getY(i));
+                }
+            } else {
+                this.renderer.getJniBridge().handleTouch(e.getPointerId(e.getActionIndex()), normalizeMotionAction(e.getActionMasked()), e.getX(e.getActionIndex()), e.getY(e.getActionIndex()));
+            }
         }
         return true;
     }
