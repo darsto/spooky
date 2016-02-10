349034a55e90639532ffad39b108642a61dd14fc
dar <darek.stojaczyk@gmail.com>
2016-02-10 19:59:38 +0100

    Set fixture to sensor if block is not opaque

--- src/core/map/block/SimpleBlock.h
+++ src/core/map/block/SimpleBlock.h
@@ -13,7 +13,10 @@ class SimpleBlock : public Block {
 public:
     SimpleBlock(Map *map, int texPos, int x, int y) : Block(map, x, y), texPos(texPos) {
         shape.SetAsBox(0.5, 0.5);
-        body->CreateFixture(&shape, 1.0f);
+        b2FixtureDef fixDef;
+        fixDef.shape = &shape;
+        fixDef.isSensor = texPos < 4 || texPos >= 16;
+        body->CreateFixture(&fixDef);
     };
 
     int getTexPos() const {
