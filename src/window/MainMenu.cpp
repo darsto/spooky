2b0695e4e56166c8e2cd7abb9fd51e9a90420133
dar <darek.stojaczyk@gmail.com>
2016-02-18 04:45:24 +0100

    Changed gui buttons gui texture

--- src/window/MainMenu.cpp
+++ src/window/MainMenu.cpp
@@ -17,7 +17,7 @@
 #endif //__ANDROID__
 
 MainMenu::MainMenu(std::function<bool(Window *window)> switchWindow) : Window(switchWindow) {
-    GuiButton *b = new GuiButton(GUI_MIDDLE_CENTER, 0, -100, 150, 75, 0);
+    GuiButton *b = new GuiButton(GUI_MIDDLE_CENTER, 0, -100, 150, 75, new int[2] {3, 11}, 2);
     auto moveController = [&](const TouchPoint *const p) {
         if (p->state == 1) {
             if (b->canBeClicked(p)) {
@@ -28,9 +28,9 @@ MainMenu::MainMenu(std::function<bool(Window *window)> switchWindow) : Window(sw
     };
     b->setOnClickListener(moveController);
     this->guiElements.push_back(b);
-    b = new GuiButton(GUI_MIDDLE_CENTER, 0, 0, 150, 75, 0);
+    b = new GuiButton(GUI_MIDDLE_CENTER, 0, 0, 150, 75, new int[2] {3, 11}, 2);
     this->guiElements.push_back(b);
-    b = new GuiButton(GUI_MIDDLE_CENTER, 0, 100, 150, 75, 0);
+    b = new GuiButton(GUI_MIDDLE_CENTER, 0, 100, 150, 75, new int[2] {3, 11}, 2);
     this->guiElements.push_back(b);
     GuiText *t = new GuiText(std::string("Dev Build: ") + __DATE__ + " " + __TIME__, 15, 15, GUI_BOTTOM_LEFT, 32, 0, 0);
     this->guiElements.push_back(t);
