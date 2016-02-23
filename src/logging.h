f883481f0aa453fb7282e16019a355bc41b66bd0
dar <darek.stojaczyk@gmail.com>
2016-02-23 18:15:23 +0100

    Finished off debug logging support

--- src/logging.h
+++ src/logging.h
@@ -14,8 +14,6 @@
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #ifdef __DEBUG__
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
-#else //__DEBUG__
-#define LOGD(...)
 #endif //__DEBUG__
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 
@@ -23,9 +21,16 @@
 
 #define  LOGE(...)  printf(__VA_ARGS__)
 #define  LOGW(...)  printf(__VA_ARGS__)
+#ifdef __DEBUG__
 #define  LOGD(...)  printf(__VA_ARGS__)
+#endif //__DEBUG__
+
 #define  LOGI(...)  printf(__VA_ARGS__)
 
 #endif //__ANDROID__
+
+#ifndef __DEBUG__
+#define LOGD(...)
+#endif //__DEBUG__
 
 #endif //C003_LOGGING_H
