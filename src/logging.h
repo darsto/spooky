7f1325a4e65fe2bade45dd7af90ead7c692ddf84
dar <darek.stojaczyk@gmail.com>
2016-02-23 16:25:12 +0100

    LOGD now prints text only when __DEBUG__ flag is defined

--- src/logging.h
+++ src/logging.h
@@ -12,7 +12,11 @@
 #define  LOG_TAG    "SpookyTom"
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#ifdef __DEBUG__
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#else //__DEBUG__
+#define LOGD(...)
+#endif //__DEBUG__
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 
 #else //__ANDROID__
