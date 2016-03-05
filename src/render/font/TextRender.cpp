a7304a1f9ace2c99b3313da1f6f0e830bc0487c2
dar <darek.stojaczyk@gmail.com>
2016-03-05 14:40:24 +0100

    Don't render text if it's invisible

--- src/render/font/TextRender.cpp
+++ src/render/font/TextRender.cpp
@@ -8,55 +8,57 @@
 TextRender::TextRender() : GuiElementRender("font", "font") { }
 
 void TextRender::render(const GuiElement *const element, glm::mat4 projectionMatrix, glm::mat4 viewMatrix, double scale) {
-    const GuiText *const text = (const GuiText *const) element;
-    texture.bindTexture(0);
-    this->shaderProgram.useProgram();
-    this->shaderProgram.setUniform("projectionMatrix", projectionMatrix);
-    this->shaderProgram.setUniform("gSampler", texture.getBoundId());
+    if (element->isVisible()) {
+        const GuiText *const text = (const GuiText *const) element;
+        texture.bindTexture(0);
+        this->shaderProgram.useProgram();
+        this->shaderProgram.setUniform("projectionMatrix", projectionMatrix);
+        this->shaderProgram.setUniform("gSampler", texture.getBoundId());
 
-    int color = element->getColor();
-    float cr = ((color & 0xFF000000) >> 24) / 255.0f;
-    float cg = ((color & 0x00FF0000) >> 16) / 255.0f;
-    float cb = ((color & 0x0000FF00) >> 8) / 255.0f;
-    float ca = (color & 0x000000FF) / 255.0f;
+        int color = element->getColor();
+        float cr = ((color & 0xFF000000) >> 24) / 255.0f;
+        float cg = ((color & 0x00FF0000) >> 16) / 255.0f;
+        float cb = ((color & 0x0000FF00) >> 8) / 255.0f;
+        float ca = (color & 0x000000FF) / 255.0f;
 
-    shaderProgram.setUniform("colorMod", glm::vec4(cr, cg, cb, ca));
+        shaderProgram.setUniform("colorMod", glm::vec4(cr, cg, cb, ca));
 
-    if (ca != 1.0f) {
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    }
-
-    scale *= text->getScale();
-
-    double x = text->getX();
-    double y = text->getY();
-    for (int i = 0; i < text->getString().length(); i++) {
-        if (text->getString().at(i) == '\n') {
-            x = text->getX();
-            y += scale + 2 * text->SPACING_PX;
-            continue;
+        if (ca != 1.0f) {
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         }
-        int texId = text->getTexPos(i);
-        if (texId == -1) {
-            x += text->TEXT_SPACESIZE * scale + text->SPACING_PX;
-            continue;
+
+        scale *= text->getScale();
+
+        double x = text->getX();
+        double y = text->getY();
+        for (int i = 0; i < text->getString().length(); i++) {
+            if (text->getString().at(i) == '\n') {
+                x = text->getX();
+                y += scale + 2 * text->SPACING_PX;
+                continue;
+            }
+            int texId = text->getTexPos(i);
+            if (texId == -1) {
+                x += text->TEXT_SPACESIZE * scale + text->SPACING_PX;
+                continue;
+            }
+            this->tmpModelMatrix = glm::translate(this->modelMatrix, glm::vec3(-(x + scale), -(y + scale), 0.0f));
+            this->tmpModelMatrix = glm::scale(this->tmpModelMatrix, glm::vec3(scale, scale, 1.0f));
+
+            shaderProgram.setUniform("modelViewMatrix", viewMatrix * this->tmpModelMatrix);
+
+            int texX = texId % atlasSize, texY = texId / atlasSize;
+            shaderProgram.setUniform("texPosX", 0.5f / texture.getWidth() + (float) texX / atlasSize);
+            shaderProgram.setUniform("texPosY", 0.5f / texture.getHeight() + (float) texY / atlasSize);
+
+            glBindVertexArray(this->vao);
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+            x += text->getGlyphSize(texId) * scale + text->SPACING_PX;
         }
-        this->tmpModelMatrix = glm::translate(this->modelMatrix, glm::vec3(-(x + scale), -(y + scale), 0.0f));
-        this->tmpModelMatrix = glm::scale(this->tmpModelMatrix, glm::vec3(scale, scale, 1.0f));
 
-        shaderProgram.setUniform("modelViewMatrix", viewMatrix * this->tmpModelMatrix);
-
-        int texX = texId % atlasSize,  texY = texId / atlasSize;
-        shaderProgram.setUniform("texPosX", 0.5f / texture.getWidth() + (float) texX / atlasSize);
-        shaderProgram.setUniform("texPosY", 0.5f / texture.getHeight() + (float) texY / atlasSize);
-
-        glBindVertexArray(this->vao);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-        x += text->getGlyphSize(texId) * scale + text->SPACING_PX;
-    }
-
-    if (ca != 1.0f) {
-        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        if (ca != 1.0f) {
+            glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+        }
     }
 }
