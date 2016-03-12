66f45ae05c1a53018c31d2d696722cb79d3d2a7e
dar <darek.stojaczyk@gmail.com>
2016-03-12 11:55:24 +0100

    Disabled VSync

--- src/render/RenderManager.cpp
+++ src/render/RenderManager.cpp
@@ -60,7 +60,7 @@ bool RenderManager::initWindow() {
                 success = false;
             } else {
                 //Use Vsync
-                if (SDL_GL_SetSwapInterval(1) < 0) {
+                if (SDL_GL_SetSwapInterval(0) < 0) {
                     printf("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
                 }
             }
