36a8e41a4a3a81341f18751533740f25389b516e
dar <darek.stojaczyk@gmail.com>
2016-03-16 21:29:50 +0100

    Father no longer collides with doors

--- src/core/map/entity/EntityFather.h
+++ src/core/map/entity/EntityFather.h
@@ -6,12 +6,21 @@
 #define C003_ENTITYFATHER_H
 
 #include "EntityMoving.h"
+#include "EntityDoor.h"
 
 class EntityFather : public EntityMoving {
 
 public:
     EntityFather(Map *map);
     virtual void update();
+
+    virtual bool doesCollide(IPositionable *obj) override {
+        if (EntityDoor *d = dynamic_cast<EntityDoor *>(obj)) {
+            return false;
+        } else {
+            return Entity::doesCollide(obj);
+        }
+    }
 
 protected:
     virtual double getSpeed();
