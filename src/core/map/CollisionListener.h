d40a6bb33f05f64d112b83f6d4d4642ee5e2f3cf
dar <darek.stojaczyk@gmail.com>
2016-03-18 10:23:04 +0100

    Added angle check when triggering collision damage. Only perpendicular impacts will be handled.

--- src/core/map/CollisionListener.h
+++ src/core/map/CollisionListener.h
@@ -19,6 +19,19 @@ public:
         triggerContact(contact, 1);
     }
 
+    double getRelativeAngle(double angle1, double angle2, double dmg_x, double dmg_y) {
+        while (angle1 > M_PI_2) { angle1 -= M_PI_2; }
+        while (angle2 > M_PI_2) { angle2 -= M_PI_2; }
+        while (angle1 < -M_PI_2) { angle1 += M_PI_2; }
+        while (angle2 < -M_PI_2) { angle2 += M_PI_2; }
+        double angle = angle1 - angle2;
+        if (angle < 0) angle *= -1;
+        if ((std::abs(dmg_x) < std::abs(dmg_y) && (dmg_y > 0)) ||
+            ((std::abs(dmg_x) > std::abs(dmg_y)) && (dmg_x < 0)))
+            angle = M_PI_2 - angle;
+        return angle;
+    }
+
     virtual void PreSolve(b2Contact *contact, const b2Manifold *oldManifold) override {
         b2ContactListener::PreSolve(contact, oldManifold);
         IPositionable *bodyUserData = static_cast<IPositionable *>(contact->GetFixtureA()->GetBody()->GetUserData());
@@ -29,7 +42,11 @@ public:
                 double dmg_x = t->getBody()->GetLinearVelocity().x;
                 double dmg_y = t->getBody()->GetLinearVelocity().y;
                 double dmg = sqrt(dmg_x * dmg_x + dmg_y * dmg_y) * 0.5;
-                if (t->getHost() != nullptr) {
+                double angle2 = 0;
+                if (Entity *t = dynamic_cast<Entity *>(i == 0 ? body2UserData : bodyUserData)) {
+                    angle2 = t->getAngle();
+                }
+                if (t->getHost() != nullptr && getRelativeAngle(atan2(dmg_y, dmg_x), angle2, dmg_x, dmg_y) < M_PI_4 / 3) {
                     t->damage(dmg);
                     t->resetMovingTime();
                 }
