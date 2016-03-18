4f6835f42396335dfb9d16ea146ba63f057b63eb
dar <darek.stojaczyk@gmail.com>
2016-03-18 10:49:20 +0100

    Added PI defines for MXE compiler

--- src/core/map/CollisionListener.h
+++ src/core/map/CollisionListener.h
@@ -9,6 +9,17 @@
 #include <core/map/entity/Entity.h>
 #include <core/map/entity/EntityToy.h>
 
+//TODO
+#ifndef M_PI
+#define M_PI		3.14159265358979323846
+#endif
+#ifndef M_PI_2
+#define M_PI_2		1.57079632679489661923
+#endif
+#ifndef M_PI_4
+#define M_PI_4		0.78539816339744830962
+#endif
+
 class CollisionListener : public b2ContactListener {
 public:
     void BeginContact(b2Contact *contact) {
