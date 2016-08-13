8380b63203f9d807a50fdac08ffc0d8327523208
dar <darek.stojaczyk@gmail.com>
2016-08-13 20:57:43 +0200

    Add const char *msg as first argument in logging functions

--- src/logging.h
+++ src/logging.h
@@ -9,40 +9,40 @@ namespace Log {
     constexpr const char *LOG_TAG = "SpookyTom";
 
     template<typename... Args>
-    constexpr const void info(Args &&...args) {
+    constexpr const void info(const char *msg, Args &&...args) {
 #ifdef __ANDROID__
-        __android_log_print(ANDROID_LOG_INFO, LOG_TAG, std::forward<Args>(args)...)
+        __android_log_print(ANDROID_LOG_INFO, LOG_TAG, msg, std::forward<Args>(args)...)
 #else
-        printf(std::forward<Args>(args)...);
+        printf(msg, std::forward<Args>(args)...);
 #endif
     };
 
     template<typename... Args>
-    constexpr const void debug(Args &&...args) {
+    constexpr const void debug(const char *msg, Args &&...args) {
 #ifdef DEBUG
 #ifdef __ANDROID__
-        __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, std::forward<Args>(args)...)
+        __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, msg, std::forward<Args>(args)...)
 #else
-        printf(std::forward<Args>(args)...);
+        printf(msg, std::forward<Args>(args)...);
 #endif
 #endif
     };
 
     template<typename... Args>
-    constexpr const void warning(Args &&...args) {
+    constexpr const void warning(const char *msg, Args &&...args) {
 #ifdef __ANDROID__
-        __android_log_print(ANDROID_LOG_WARN, ANDROID_LOG_DEBUG, std::forward<Args>(args)...)
+        __android_log_print(ANDROID_LOG_WARN, ANDROID_LOG_DEBUG, msg, std::forward<Args>(args)...)
 #else
-        printf(std::forward<Args>(args)...);
+        printf(msg, std::forward<Args>(args)...);
 #endif
     };
 
     template<typename... Args>
-    constexpr const void error(Args &&...args) {
+    constexpr const void error(const char *msg, Args &&...args) {
 #ifdef __ANDROID__
-        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, std::forward<Args>(args)...)
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, msg, std::forward<Args>(args)...)
 #else
-        printf(std::forward<Args>(args)...);
+        printf(msg, std::forward<Args>(args)...);
 #endif
     };
 };
