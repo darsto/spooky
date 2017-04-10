2a180777065d2a67d2126c8f8390f6280de0bd50
dar <darek.stojaczyk@gmail.com>
2017-04-10 17:05:49 +0200

    Updated Copyright headers

--- src/render/gui/GuiElementRender.cpp
+++ src/render/gui/GuiElementRender.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Dariusz Stojaczyk. All Rights Reserved.
+ * Copyright (c) 2017 Dariusz Stojaczyk. All Rights Reserved.
  * The following source code is released under an MIT-style license,
  * that can be found in the LICENSE file.
  */
