b2d83ab805f054d4acb21902dc3b4bbad53c046d
dar <darek.stojaczyk@gmail.com>
2017-04-17 21:14:45 +0200

    Added documentation of WindowManager

--- src/window/WindowManager.h
+++ src/window/WindowManager.h
@@ -10,11 +10,31 @@
 #include "Window.h"
 #include "render/window/WindowRender.h"
 
+/**
+ * A wrapper for Window and WindowRender getters.
+ * This is a pure virtual class meant to be implemented by the user.
+ */
 class WindowManager {
 public:
+    /**
+     * Get Window of given id.
+     * @param index unique identifier of a window to get
+     * @return pointer to the Window or nullptr.
+     * On nullptr, the Application will safely exit.
+     */
     virtual Window *getWindow(int index) = 0;
+    
+    /**
+     * Get WindowRender of given id.
+     * @param index unique identifier of a window to get
+     * @return pointer to the WindowRender.
+     * Cannot be nullptr.
+     */
     virtual WindowRender *getWindowRender(int index) = 0;
     
+    /**
+     * Identifier used in above methods on initialization.
+     */
     static constexpr const int INITIAL_WINDOW_ID = 0;
 };
 
